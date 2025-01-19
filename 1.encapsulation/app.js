efd864c8224655b571f06d744b883a3ac5070446
uzairahmed2126 <uzairahmed21261@gmail.com>
2025-01-20 00:43:52 +0530

    encapsulation

--- 1.encapsulation/app.js
+++ 1.encapsulation/app.js
@@ -1,9 +1,24 @@
-const user = {
+const user1 = {
   name: "joe",
   score: 0,
   increment: function () {
     this.score++;
   },
 };
-let incrementBy1 = user.increment();
-console.log(user);
+let incrementBy1 = user1.increment();
+console.log(user1);
+
+const user = {
+  name: "Julia",
+  score: 10,
+  increment() {
+    this.score++;
+  },
+  details: {
+    email: "julia@example.com",
+    address: "123 Main St"
+  }
+};
+
+user.increment();  // Increases score to 11
+console.log(user.details.email);  // Access nested object
